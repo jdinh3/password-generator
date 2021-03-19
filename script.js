7e77c842bc788fee34805b45ab68a5042543014d
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-03-18 20:53:59 -0500

    added prompt for password length in js

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 // Assignment Code
-console.log("hello world")
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); 
+
+// generates prompts when button is pressed
+generateBtn.addEventListener("click", promptMe);
+  function promptMe() {
+    var pwLength = prompt("How many characters would you like your password to contain?");
+    
+
+// // Conditional Statements if user inputs an invalid password length
+if (pwLength < 8) {
+  alert("Password length must be at least 8 characters");
+} else if
+  (pwLength > 128) {
+  alert("Password must be less than 129 characters");
+}
+
+console.log(pwLength);
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  function generatePassword() {
+    return "temporary password";
+    }
 
+  passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
